39824d1fabf966fb8583a754d41fdd54c56b9f45
weeming <2655834088@qq.com>
2021-12-24 09:16:51 +0800

    UDP

---
+++ src/main/java/com/lwm/chapter12/client/ChineseProverbClientHandler.java
@@ -0,0 +1,30 @@
+package com.lwm.chapter12.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author : lwm
+ * @date : 2021/12/23 22:58
+ * @description : com.lwm.chapter12.client
+ * @version : 1.0
+ */
+public class ChineseProverbClientHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+        String response = msg.content().toString(CharsetUtil.UTF_8);
+        if (response.startsWith("query result")) {
+            System.out.println(response);
+            ctx.close();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
